529b813db89a918a64a8c8033a1e549b92a89c3f
Ethan Knowlton <eknowlton@gmail.com>
2019-04-28 13:33:06 -0400

    polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,7 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+export MONITOR_PRIMARY=eDP1
+
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar top &
+polybar primary-top &
+polybar primary-bottom &
+
 
 echo "Polybar launched..."
